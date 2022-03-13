ed9204e609e0eee7bca8903037623c51206419e4
Noah <noahmhodgson@gmail.com>
2022-03-13 16:56:12 -0400

    unigrams and bigrams are printed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys
-import re
-from nltk.tokenize import sent_tokenize
+from nltk import sent_tokenize, ngrams
 
 def main():
         REVIEW_FILE = "LOTR_REVIEW.txt"
@@ -8,12 +7,30 @@ def main():
         review = open(REVIEW_FILE, "r")
         review = review.read()
         review_sent = sent_tokenize(review)
-        print(review_sent)
+        review_unigram = ngrams(review.split(), 1)
+        for i in review_unigram:
+            print(i, end='')
+        review_bigram = ngrams(review.split(), 2)
+        print("------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------")
         print("------------------------------------------------------------------------------")
         wiki = open(WIKI_FILE, "r")
         wiki = wiki.read()
         wiki_sent = sent_tokenize(wiki)
-        print(wiki_sent)
+        wiki_unigram = ngrams(wiki.split(), 1)
+        for i in wiki_unigram:
+            print(i, end='')
+        wiki_bigram = ngrams(wiki.split(), 2)
+        print("------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------")
+        for i in review_bigram:
+            print(i, end='')
+        print("------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------")
+        for i in wiki_bigram:
+            print(i, end='')
         quit()
 
 
