b15288168499d20a4105c26b065ca109bddae4f8
Noah <noahmhodgson@gmail.com>
2022-03-15 15:11:41 -0400

    test

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from collections import Counter
+from random import random
 import sys
 from nltk import sent_tokenize, ngrams
+
+#uses add 1 smoothing
+def process_unigrams_prob(sent: str, corp:Counter) -> int:
+    val = 0
+    sent = list(set(sent.split()))
+    words = ngrams(sent, 1)
+    for word in words:
+        val += 1 + corp[word]
+    print(val)
+    return val
+
+#uses add 1 smoothing
+def proccess_bigram_prob(sent: str, corp:Counter) -> int:
+    val = 1   
+    biwords = ngrams(sent.split(), 2)
+    for biword in biwords:
+        val += corp[biword]
+    print(val)
+    return val
 
 def main():
         REVIEW_FILE = "LOTR_REVIEW.txt"
@@ -36,17 +56,29 @@ def main():
         print("------------------------------------------------------------------------------")
         print("------------------------------------------------------------------------------")
         print("Most common unigrams for wiki:")
-        cnt_wiki = Counter(ngrams(wiki.split(), 1))
-        print(cnt_wiki.most_common(30))
+        cnt_wiki1 = Counter(ngrams(wiki.split(), 1))
+        print(cnt_wiki1.most_common(30))
         print("Most common bigrams for wiki:")
         cnt_wiki = Counter(ngrams(wiki.split(), 2))
         print(cnt_wiki.most_common(30))
         print("Most common unigrams for reviews:")
-        cnt_rev = Counter(ngrams(review.split(), 1))
-        print(cnt_rev.most_common(30))
+        cnt_rev1 = Counter(ngrams(review.split(), 1))
+        print(cnt_rev1.most_common(30))
         print("Most common bigrams for reviews:")
         cnt_rev = Counter(ngrams(review.split(), 2))
         print(cnt_rev.most_common(30))
+        print("------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------")
+        print("------------------------------------------------------------------------------")
+        #add one smoothing goes here 
+        random_rev_set = "To the tear-choked death of Boromir, to the triumph of Gondor, Lord of the Rings has never failed in overripe entertainment and overall attraction."
+        random_wiki_set = "There, Boromir tries to take the Ring from Frodo, but immediately regrets it after Frodo puts on the Ring and disappears."
+        vocab_len = len(list(cnt_rev1)) + len(list(cnt_wiki1))
+        process_unigrams_prob(random_wiki_set, cnt_wiki1)
+        proccess_bigram_prob(random_wiki_set, cnt_wiki)
+        process_unigrams_prob(random_rev_set, cnt_rev1)
+        proccess_bigram_prob(random_rev_set, cnt_rev)
+        print(vocab_len)
         quit()
 
 
